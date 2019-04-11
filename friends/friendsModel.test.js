0e465cbc7bda59a80e6b641b45d9833eb75e0263
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-11 15:43:27 +0100

    MVP: Write tests for helpers

---
+++ friends/friendsModel.test.js
@@ -0,0 +1,38 @@
+const db = require('../data/dbConfig.js');
+const Friends = require('./friendsModel');
+
+describe('friendsModel', () => {
+  beforeEach(async () => {
+    await db('friends').truncate();
+    await Friends.insert({ name: 'Monica' });
+  });
+
+  describe('insert', () => {
+    it('should insert the given friend into the db', async () => {
+      const friend = await Friends.insert({ name: 'Chandler' });
+      const row = await db('friends');
+      expect(friend.name).toBe('Chandler');
+      expect(row).toHaveLength(2);
+    });
+    it('should insert the given friends into the db', async () => {
+      await Friends.insert({ name: 'Joey' });
+      await Friends.insert({ name: 'Ross' });
+
+      const friendsList = await db('friends');
+      expect(friendsList).toHaveLength(3);
+    });
+  });
+
+  describe('delete', () => {
+    it('should delete a given friend by its ID', async () => {
+      const result = await Friends.remove(1);
+      const row = await db('friends');
+      expect(row).toHaveLength(0);
+    });
+    it('should return 0 if given friend ID could not be found', async () => {
+      const expected = 0;
+      const actual = await Friends.remove(2);
+      expect(actual).toEqual(expected);
+    });
+  });
+});
